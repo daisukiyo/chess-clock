ab977d5c21c57fe78120b5dc49bb56948851566f
daisukiyo <tk.rain09@gmail.com>
2019-05-09 15:08:34 -0700

    hardcoded values and styled the component

--- components/Timer.js
+++ components/Timer.js
@@ -3,9 +3,13 @@ import { StyleSheet, Text, View } from 'react-native'
 
 const Timer = (props) => {
     return (
-        <View style={styles.container}>
-            <Text style={styles.time}>Time: 00:00</Text>
-            <Text style={styles.counter}>Count: 0</Text>
+        // Component styles applied from the component itself
+        // Unique styles are passed through App.js via props
+        <View style={{...styles.container, ...props.styling}}>
+            {/* Hardcoded Time Values */}
+            <Text style={styles.time}>5:00</Text>
+            {/* Hardcoded Move Counter */}
+            <Text style={styles.counter}>0</Text>
         </View>
     )
 }
@@ -13,6 +17,25 @@ const Timer = (props) => {
 export default Timer
 
 const styles = StyleSheet.create({
+    container: {
+        backgroundColor:  '#ff6b6b',
+    },
+    time: {
+        // text styling
+        color: 'rgb(255, 255, 255)',
+        fontFamily: 'System',
+        fontSize: 144,
+    },
+    counter: {
+        // text styling
+        color: 'rgba(255, 255, 255, 0.5)', 
+        fontFamily: 'System',
+        fontSize: 32,
 
+        // absolute positioning for bottom right hand corner
+        position: 'absolute',
+        bottom: 4,
+        right: 6,
+    }
 })
 
